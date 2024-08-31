06b76593b35fb7ecfb66568e4e20b7f475621f1c
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2024-08-31 18:44:19 +0400

    Add navigation mockup, further develope welcome section, fix minor bugs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import {
   CardOverflow,
   CardContent,
   Typography,
-  // Link,
+  Stack,
+  Link,
   AspectRatio,
   Button,
   Chip,
@@ -27,6 +28,36 @@ function App() {
 
   return (
     <div className="App">
+      <Box
+        sx={{
+          width: "100%",
+          pt: "1%",
+          position: "absolute",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          top: "0",
+        }}
+      >
+        <img
+          src="src/assets/recipeBook.png"
+          loading="lazy"
+          alt="Recipe book icon"
+          style={{ width: "120px", aspectRatio: "1" }}
+        />
+        <Stack direction="row" spacing={1}>
+          <Link
+            underline="hover"
+            sx={{
+              fontSize: "1.4rem",
+              color: "rgb(255, 255, 255)",
+              textTransform: "uppercase",
+            }}
+          >
+            Recipes
+          </Link>
+        </Stack>
+      </Box>
       <Sheet
         sx={{
           position: "relative",
@@ -39,19 +70,50 @@ function App() {
         }}
       >
         <GradientBack />
+        <div className="custom-shape-divider-bottom-1725112114">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            />
+          </svg>
+        </div>
+
         <Typography
           level="h1"
           sx={{
-            fontSize: "5rem",
+            fontSize: "8rem",
             color: "rgba(225, 225, 225, 0.5)",
             zIndex: "1",
             textTransform: "uppercase",
-            weight: "900",
+            fontWeight: "900",
           }}
         >
-          Recipe Book
+          Recipe
         </Typography>
-        <Typography level="title-lg" sx={{ color: "rgba(225, 225, 225, 0.5)" }}>
+        <Typography
+          level="h2"
+          sx={{
+            fontSize: "3.6rem",
+            color: "rgb(255, 255, 255)",
+            textTransform: "uppercase",
+            fontWeight: "800",
+            transform: "translate(-50%, -105%)",
+          }}
+        >
+          Book
+        </Typography>
+        <Typography
+          level="title-lg"
+          sx={{
+            color: "rgba(225, 225, 225, 0.5)",
+          }}
+        >
           Recipes from all over the world
         </Typography>
       </Sheet>
@@ -60,7 +122,6 @@ function App() {
         sx={{
           flexGrow: 1,
           pt: "1rem",
-          borderTop: "5px solid grey",
           justifyContent: "center",
         }}
       >
